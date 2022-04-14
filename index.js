620df797ef57b8386f677a371588ef848bae93a9
Matthew Bieda <63665300+MatthewBieda@users.noreply.github.com>
2022-04-14 11:51:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function IncrementScore(id) {
+    userscore = document.getElementById("Score").innerHTML
+    
+    var clickedpicture = document.getElementById(id)
+    let clickstate = clickedpicture.getAttribute("data-clicked");
+    console.log(clickstate)
+
+    scoreasint = parseInt(userscore)
+
+    if (id.includes("egg") && clickstate=="False") {
+        scoreasint += 1;
+        if (scoreasint == 10) {
+            alert("You Win!!")
+        }
+        clickedpicture.setAttribute("data-clicked", "True")
+    } else if (id.includes("carrot") && clickstate=="False") {
+        clickedpicture.setAttribute("data-clicked", "True")
+        scoreasint -= 1;
+    }
+
+    document.getElementById("Score").innerHTML = scoreasint
+    
+    eggnumber = id
+    egg = document.getElementById(eggnumber)
+    egg.style.opacity = 1;
+
+    
+}
